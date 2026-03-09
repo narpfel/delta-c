2c1df89e31e61bd6a316b653ed628b4cb6ed757c
Paul Gey <narpfel@gmx.de>
2026-03-09 20:51:25 +0100

    implement basic diff functionality

---
+++ Δc.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+from collections import defaultdict
+from collections import namedtuple
+from difflib import SequenceMatcher
+from functools import partial
+from itertools import takewhile
+from operator import attrgetter
+from pathlib import Path
+
+SOURCE_LINE_RE = re.compile(r"^\s*(?P<lineno>\d+)\|\s*(?P<count>[^|\s]*)\s*\|(?P<text>.*)$")
+
+Line = namedtuple("Line", "line, lineno, count, is_covered, text")
+
+
+class CompareBy(namedtuple("CompareBy", "keys, item")):
+    def __eq__(self, other):
+        if not isinstance(other, CompareBy) or self.keys != other.keys:
+            return NotImplemented
+        key = attrgetter(*self.keys)
+        return key(self.item) == key(other.item)
+
+    def __hash__(self):
+        return hash(attrgetter(*self.keys)(self.item))
+
+
+class DiffLine(namedtuple("DiffLine", "marker, line, tag")):
+    def __str__(self):
+        return f"{self.marker}{self.line}"
+
+
+def parse(lines):
+    files = defaultdict(list)
+    filename = None
+    for line in lines:
+        line = line.rstrip()
+        if line.startswith("/") and line.endswith(":"):
+            filename = Path(line.removesuffix(":"))
+        else:
+            assert filename is not None
+
+            match = SOURCE_LINE_RE.fullmatch(line)
+            if match is not None:
+                files[filename].append(
+                    Line(
+                        line=match[0],
+                        lineno=int(match["lineno"]),
+                        count=match["count"],
+                        is_covered=match["count"] != "0",
+                        text=match["text"],
+                    ),
+                )
+
+    return files
+
+
+def context(lines):
+    return takewhile(lambda line: line.marker == " ", lines)
+
+
+def diff(filename, left, right):
+    comparer = partial(CompareBy, ("text", "is_covered"))
+    matcher = SequenceMatcher(
+        None,
+        list(map(comparer, left)),
+        list(map(comparer, right)),
+    )
+
+    show_header = True
+    for opcodes in matcher.get_grouped_opcodes():
+        lines = []
+        for tag, l_from, l_to, r_from, r_to in opcodes:
+            if tag == "equal":
+                for line in right[r_from:r_to]:
+                    lines.append(DiffLine(marker=" ", line=line.line, tag=tag))
+            elif tag in {"replace", "insert"}:
+                for line in right[r_from:r_to]:
+                    marker = " " if line.is_covered else "+"
+                    lines.append(DiffLine(marker=marker, line=line.line, tag=tag))
+
+        start_offset = sum(1 for _ in context(lines)) - 3
+        end_offset = sum(1 for _ in context(reversed(lines))) - 3
+
+        lines = lines[start_offset:len(lines) - end_offset]
+        if not lines:
+            continue
+
+        if show_header:
+            show_header = False
+            yield f"--- a/{filename}"
+            yield f"+++ b/{filename}"
+
+        _, left_start, _, right_start, _ = opcodes[0]
+
+        yield f"@@ -{left_start + 1},1 +{right_start + start_offset + 1},{len(lines)} @@"
+        yield from lines
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("left", type=Path)
+    parser.add_argument("right", type=Path)
+    parser.add_argument("--relative-to", type=lambda arg: Path(arg).resolve(), default=Path.cwd())
+    args = parser.parse_args(args)
+
+    with args.left.open() as file:
+        left = parse(file)
+    with args.right.open() as file:
+        right = parse(file)
+
+    for (filename, left_lines), right_lines in zip(left.items(), right.values()):
+        lines = diff(
+            filename.relative_to(args.relative_to, walk_up=True),
+            left_lines,
+            right_lines,
+        )
+        for line in lines:
+            print(line)
+
+
+if __name__ == "__main__":
+    main()
